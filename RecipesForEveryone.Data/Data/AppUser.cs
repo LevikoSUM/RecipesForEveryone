895a1e8c633e1a760be7a7a5b38880254e32926e
Leviko <kosta.ilev@gmail.com>
2024-04-10 23:26:09 +0300

    add AppUser

---
+++ RecipesForEveryone.Data/Data/AppUser.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipesForEveryone.Data.Data
+{
+    public class AppUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public virtual ICollection<Recipe>? Recipes { get; set; }
+        public virtual ICollection<Comment>? Comments { get; set; }
+    }
+}

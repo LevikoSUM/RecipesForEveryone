21855f9ac8befc8f1778a5b04a175cf61c2fccba
Leviko <kosta.ilev@gmail.com>
2024-04-10 23:26:55 +0300

    add RecipeType

---
+++ RecipesForEveryone.Data/Data/Enums/RecipeType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipesForEveryone.Data.Data.Enums
+{
+    public enum RecipeType
+    {
+        OTHER = 0,
+        PASTA = 1,
+        DRINKS = 2,
+        SALAD = 3,
+        FISH = 4,
+        BEEF = 5,
+        CHICKEN = 6,
+        PORK = 7,
+        SOUP = 8
+    }
+}

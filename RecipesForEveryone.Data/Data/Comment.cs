cc36b938d31513496a02e6a1d72ea5f42e34d07c
Leviko <kosta.ilev@gmail.com>
2024-04-10 23:26:30 +0300

    add Comment

---
+++ RecipesForEveryone.Data/Data/Comment.cs
@@ -0,0 +1,19 @@
+﻿using RecipesForEveryone.Data.Data.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipesForEveryone.Data.Data
+{
+    public class Comment : BaseEntity
+    {
+        public string Content { get; set; }
+        public string UserID { get; set; }
+        public int RecipeID { get; set; }
+
+        public virtual AppUser? User { get; set; }
+        public virtual Recipe? Recipe { get; set; }
+    }
+}

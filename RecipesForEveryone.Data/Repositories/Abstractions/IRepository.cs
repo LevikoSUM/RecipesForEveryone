8033971bfbe102d33aa5e61e133a6abf4bceca19
Leviko <kosta.ilev@gmail.com>
2024-04-11 22:26:18 +0300

    add repository

---
+++ RecipesForEveryone.Data/Repositories/Abstractions/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using RecipesForEveryone.Data.Data.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipesForEveryone.Data.Repositories.Abstractions
+{
+    public interface IRepository<T>
+        where T : BaseEntity
+    {
+        public Task AddAsync(T entity);
+        public Task<IEnumerable<T>> GetAsync(Expression<Func<T, bool>> predicate);
+        public Task<IEnumerable<T>> GetAllAsync();
+        public Task<T> GetByIdAsync(int id);
+        public Task UpdateAsync(T entity);
+        public Task DeleteByIdAsync(int id);
+    }
+}
---
+++ RecipesForEveryone.Data/Repositories/Repository.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RecipesForEveryone.Data.Data.Abstractions;
+using RecipesForEveryone.Data.Repositories.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipesForEveryone.Data.Repositories
+{
+    public class Repository<T> : IRepository<T>
+        where T : BaseEntity
+    {
+        private readonly DbContext _context;
+        private readonly DbSet<T> _dbSet;
+        public Repository(DbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+        public async Task AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteByIdAsync(int id)
+        {
+            var entity = await GetByIdAsync(id);
+            if (entity != null)
+            {
+                _dbSet.Remove(entity);
+                await _context.SaveChangesAsync();
+            }
+            else
+            {
+                throw new InvalidOperationException($"Item does not exist: {id}");
+            }
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<IEnumerable<T>> GetAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _dbSet.Where(predicate)
+                .ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+            _dbSet.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+}

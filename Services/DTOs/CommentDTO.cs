e79610ee99a2f52cc46499a69e490dd97881d212
Leviko <kosta.ilev@gmail.com>
2024-04-12 22:24:36 +0300

    add DTOs

---
+++ Services/DTOs/CommentDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.DTOs
+{
+    public class CommentDTO : BaseDTO
+    {
+        public string Content { get; set; }
+        public string UserId { get; set; }
+        public int RecipeId { get; set; }
+    }
+}
---
+++ Services/DTOs/RecipeDTO.cs
@@ -0,0 +1,22 @@
+﻿using RecipesForEveryone.Data.Data.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.DTOs
+{
+    public class RecipeDTO : BaseDTO
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Ingredients { get; set; }
+        public string Instructions { get; set; }
+        public bool IsPublic { get; set; }
+        public byte[] Image { get; set; }
+        public string UserId { get; set; }
+        public RecipeType RecipeType { get; set; }
+        public string RecipeTypeName { get; set; } 
+    }
+}

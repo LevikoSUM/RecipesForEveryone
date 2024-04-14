c8ab51b30c947c5808498bb10bb21dad964ce7b0
Leviko <kosta.ilev@gmail.com>
2024-04-14 04:24:10 +0300

    add db context factory

---
+++ RecipesForEveryone.Data/Data/DbContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipesForEveryone.Data.Data
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=EcommerceDb;Trusted_Connection=True;MultipleActiveResultSets=true");
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
